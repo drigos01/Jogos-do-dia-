985a67d9480bc663eba287d91306094e64e1bb6f
drigos01 <helciorodri@gmail.com>
2025-11-02 21:00:38 -0300

    Update geminiService.ts

--- services/geminiService.ts
+++ services/geminiService.ts
@@ -2,7 +2,7 @@
 import { GoogleGenAI } from "@google/genai";
 import { Game, PastGame, AiAnalysisResponse } from '../types';
 
-const API_KEY = process.env.API_KEY;
+const API_KEY = process.env.jogosdodia;
 
 if (!API_KEY) {
   throw new Error("API_KEY environment variable not set");
